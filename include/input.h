b538dbeaa95bf9340689cb3733255c1f79ebb4ad
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-06-29 10:35:30 -0300

    feat: leitura de botões e manipulação do buffer de entrada

---
+++ include/input.h
@@ -0,0 +1,23 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#define PASSWORD_SIZE 5
+
+#include "utils.h"
+
+typedef struct {
+  u8 buffer[PASSWORD_SIZE]; // Dígitos da senha
+  u8 length;                // Qtd dígitos inseridos
+  u32 last_modified;        // Tempo desde última modificação no buffer
+} Input;
+
+
+void inputInit();           // Configuração dos botões e timers
+void inputUpdate();         // Atualiza input conforme regras de timeout
+void inputEnable();         // Ativa interrupção de botão
+void inputDisable();        // Desativa interrupções de botão
+volatile u8* inputBuffer();          // Retorna o buffer com os dígitos inseridos
+u8 inputIsActive();          // Retorna 1 se input não foi vazio por mais de 3s, 0 caso contrário
+u8 inputLength();           // Retorna qtd dígitos inseridos 
+
+#endif // INPUT_H
---
+++ src/input.c
@@ -0,0 +1,112 @@
+#include <msp430.h>
+#include "input.h"
+
+static volatile Input input;
+
+void inputInit(void)
+{
+    // Configuração botões P1.1 e P2.1
+
+    // Entrada
+    P1DIR &= ~BIT1;   
+    P2DIR &= ~BIT1;
+
+    // Resistor interno
+    P1REN |= BIT1;
+    P2REN |= BIT1;
+    P1OUT |= BIT1; // pull-up
+    P2OUT |= BIT1; // --
+
+    // Configuração de timer para debounce
+    TA1CCTL0 = CCIE;
+    TA1CCR0 = 5000;
+    TA1CTL = TASSEL_2 + MC_0 + TACLR;
+}
+
+void inputUpdate(void)
+{
+    inputDisable();
+    if (input.length && timeout(input.last_modified, 3000))
+    {
+        input.length = 0;               // Limpa o buffer
+        input.last_modified = milis();  // Vazio desde agora
+    }
+    inputEnable();
+}
+
+void inputEnable(void)
+{
+    // Interrupções na borda de descida dos botões
+    P1IES |= BIT1;
+    P1IFG &= ~BIT1;
+    P1IE  |= BIT1;
+    
+    P2IES |= BIT1;
+    P2IFG &= ~BIT1;
+    P2IE  |= BIT1;
+}
+
+void inputDisable(void)
+{
+    P2IE &= ~BIT1;
+    P1IE &= ~BIT1;
+}
+
+volatile u8* inputBuffer(void)
+{
+    return input.buffer;
+}
+
+u8 inputIsActive(void)
+{
+    return !(input.length == 0 && timeout(input.last_modified, 30000));
+}
+
+u8 inputLength(void)
+{
+    return input.length;
+}
+
+#pragma vector = TIMER0_A1_VECTOR
+__interrupt void debounce(void) {
+    TA1CTL = MC_0;    // Para o timer
+
+    // Evitar overflow de buffer
+    if (input.length >= PASSWORD_SIZE) return;
+
+    inputEnable();    // Reabilita interrupções
+
+    // Botão ainda pressionado? registrar evento
+    // (else if para invalidar quando dois botões pressionados)
+    if ((P1IN & BIT1) == 0) 
+    {
+        input.buffer[input.length++] = 0;  
+        input.last_modified = milis();
+    } 
+    else if ((P2IN & BIT1) == 0) 
+    {    
+        input.buffer[input.length++] = 1;  
+        input.last_modified = milis();
+    }
+}
+
+#pragma vector=PORT2_VECTOR
+__interrupt void S1_ISR(void) {
+  if (P2IFG & BIT1) {
+    inputDisable();
+    P2IFG &= ~BIT1;
+
+    // Timer de debounce
+    TA1CTL = TASSEL_2 + MC_1 + TACLR;
+  }
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void S2_ISR(void) {
+  if (P1IFG & BIT1) {
+    inputDisable();
+    P1IFG &= ~BIT1;
+
+    TA1CTL = TASSEL_2 + MC_1 + TACLR;
+  }
+}
