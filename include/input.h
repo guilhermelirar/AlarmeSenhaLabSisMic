571a73143beb18ff7267157c35bd7584d5f67928
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-06-29 10:37:37 -0300

    formatação

--- include/input.h
+++ include/input.h
@@ -11,13 +11,12 @@ typedef struct {
   u32 last_modified;        // Tempo desde última modificação no buffer
 } Input;
 
-
 void inputInit();           // Configuração dos botões e timers
 void inputUpdate();         // Atualiza input conforme regras de timeout
 void inputEnable();         // Ativa interrupção de botão
 void inputDisable();        // Desativa interrupções de botão
-volatile u8* inputBuffer();          // Retorna o buffer com os dígitos inseridos
-u8 inputIsActive();          // Retorna 1 se input não foi vazio por mais de 3s, 0 caso contrário
+volatile u8* inputBuffer(); // Retorna o buffer com os dígitos inseridos
+u8 inputIsActive();         // Retorna 1 se input não foi vazio por mais de 3s, 0 caso contrário
 u8 inputLength();           // Retorna qtd dígitos inseridos 
 
 #endif // INPUT_H
